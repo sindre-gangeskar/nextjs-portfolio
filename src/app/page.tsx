a4ba4e89d22810984e32c5c46baa4ac1641d63f2
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2025-02-17 22:17:05 +0100

    Updated fetching for Vercel url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Page from "@/pages/home/Page";
 
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `http://localhost:3000`;
+
 async function fetchRepoData() {
-	const res = await fetch(`${process.env.HOST}/api/github/repos`, { cache: "force-cache" });
+	const res = await fetch(`${baseUrl}/api/github/repos`, { cache: "force-cache" });
 	const data = await res.json();
 	return data.repos;
 }
 
 async function getGithubUser() {
-	const res = await fetch(`${process.env.HOST}/api/github/user`, { cache: "force-cache" });
+	const res = await fetch(`${baseUrl}/api/github/user`, { cache: "force-cache" });
 	const data = await res.json();
 	return data;
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,17 @@
+
 import { Metadata } from "next";
 import Page from "@/pages/projects/Page";
 
 export const metadata: Metadata = {
-	title: 'Projects',
-	description: 'Explore my projects'
-}
+	title: "Projects",
+	description: "Explore my projects",
+};
 
 async function fetchProjects() {
-	const response = await fetch(`${process.env.HOST}/api/github/repos/all`, {cache: 'force-cache'});
-  const data = await response.json();
-  
+	const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `http://localhost:3000`;
+	const response = await fetch(`${baseUrl}/api/github/repos/all`, { cache: "force-cache" });
+	const data = await response.json();
+
 	return data;
 }
 
