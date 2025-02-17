b3f679a1dd36585b3cce70dca9bed870751661b2
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2025-02-17 22:37:40 +0100

    Updated projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,7 +10,7 @@ async function fetchProjects() {
 	try {
 		const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `http://localhost:3000`;
 		const response = await fetch(`${baseUrl}/api/github/repos/all`, { cache: "force-cache", headers: { "Content-Type": "application/json", accept: "application/json" } });
-		console.log(response);
+		console.log(response.status, response.statusText);
 		const data = await response.json();
 		return data;
 	} catch (error) {
