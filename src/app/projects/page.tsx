8a9a025fc8a8afd662538627f34ceb3141db32d9
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2025-02-17 22:25:56 +0100

    Updated projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,3 @@
-
 import { Metadata } from "next";
 import Page from "@/pages/projects/Page";
 
@@ -10,9 +9,10 @@ export const metadata: Metadata = {
 async function fetchProjects() {
 	const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `http://localhost:3000`;
 	const response = await fetch(`${baseUrl}/api/github/repos/all`, { cache: "force-cache" });
-	const data = await response.json();
-
-	return data;
+	if (response.ok) {
+		const data = await response.json();
+		return data;
+	} else return null;
 }
 
 export default async function Projects() {
