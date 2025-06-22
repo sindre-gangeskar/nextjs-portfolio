107b8df276030e86ba56fc671984f849d2eabea3
Sindre Gangeskar <sindre.gangeskar2710@outlook.com>
2025-06-22 17:02:18 +0200

    Add cloudflare web analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,31 +8,31 @@ import ThemeProvider from "@/components/ThemeProvider";
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
-	display: 'swap'
+	display: "swap",
 });
 
 const geistMono = Geist_Mono({
 	variable: "--font-geist-mono",
 	subsets: ["latin"],
-	display: 'swap'
+	display: "swap",
 });
 
 const publicSans = Public_Sans({
 	variable: "--font-public-sans",
 	subsets: ["latin"],
-	display: 'swap'
+	display: "swap",
 });
 
 const quicksand = Quicksand({
 	variable: "--font-quicksand",
 	subsets: ["latin"],
-	display: 'swap'
+	display: "swap",
 });
 
 const firaCode = Fira_Code({
 	variable: "--font-fira-code",
 	subsets: ["latin"],
-	display: 'swap'
+	display: "swap",
 });
 export const metadata: Metadata = {
 	title: "Sindre Gangeskar",
@@ -45,20 +45,22 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-			<html lang="en" suppressHydrationWarning={true}>
-				<head>
-					<noscript>Javascript must be enabled in order to visit this site</noscript>
-					<meta name="viewport" content="initial-scale=1, width=device-width" />
-				</head>
-				<body className={`${geistSans.variable} ${geistMono.variable} ${publicSans.variable}${quicksand.variable} ${firaCode.variable}`}>
-					<InitColorSchemeScript />
-					<CssVarsProvider defaultMode="dark">
-						<ThemeProvider>
-							<Navbar />
-							{children}
-						</ThemeProvider>
-					</CssVarsProvider>
-				</body>
-			</html>
+		<html lang="en" suppressHydrationWarning={true}>
+			<head>
+				<noscript>Javascript must be enabled in order to visit this site</noscript>
+				<script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "d0053e884cb9447f9867c0123fa2f45e"}'></script>
+
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</head>
+			<body className={`${geistSans.variable} ${geistMono.variable} ${publicSans.variable}${quicksand.variable} ${firaCode.variable}`}>
+				<InitColorSchemeScript />
+				<CssVarsProvider defaultMode="dark">
+					<ThemeProvider>
+						<Navbar />
+						{children}
+					</ThemeProvider>
+				</CssVarsProvider>
+			</body>
+		</html>
 	);
 }
