ae970f32b72c2a3212b3bd866d99b852cade0604
Sindre Gangeskar <sindre.gangeskar2710@outlook.com>
2025-10-18 15:47:42 +0200

    Add reconnection logic to MQTT experiment

--- components/experiments/mqtt/MQTTMessage.tsx
+++ components/experiments/mqtt/MQTTMessage.tsx
@@ -1,21 +1,28 @@
 "use client";
 import { Typography } from "@mui/joy";
-import mqtt from "mqtt";
-import { useState, useEffect } from "react";
+import mqtt, { MqttClient } from "mqtt";
+import { useState, useEffect, useRef } from "react";
 export default function MQTTMessage({ id }: { id?: string }) {
-	const [message, setMessage] = useState("Connecting...");
-
+	const [message, setMessage] = useState("Connecting to HiveMQ's broker...");
+	const attemptsRef = useRef(0);
+	const clientRef = useRef<MqttClient | null>(null);
 	useEffect(() => {
 		const client = mqtt.connect("wss://broker.hivemq.com:8884/mqtt");
-
+		clientRef.current = client;
 		client.on("connect", () => {
-			setMessage("Connected to broker");
+			attemptsRef.current = 0;
+			setMessage("Connected to Hive broker");
 			client.subscribe("mqtt_test_data/a6c072abf6ef07cc");
+		});
 
-			client.on("offline", () => {
-				setMessage("Failed to connect to broker");
-				client.end();
-			});
+		client.on("reconnect", () => {
+			attemptsRef.current++;
+			if (attemptsRef.current > 10) {
+				client.end(true);
+				setMessage('Failed to connect to broker.. Connection has ended.');
+				return;
+			}
+			setMessage(`Reconnecting... ${attemptsRef.current} / 10`);
 		});
 
 		client.on("message", (_, msg) => {
@@ -23,7 +30,7 @@ export default function MQTTMessage({ id }: { id?: string }) {
 		});
 
 		return () => {
-			client.end();
+			client.end(true);
 		};
 	}, []);
 
