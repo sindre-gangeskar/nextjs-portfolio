a3e00b21f6e1c347da2916ed60ca7a6d88397b6b
Sindre Gangeskar <sindre.gangeskar@outlook.com>
2025-12-01 22:12:42 +0100

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
 			}
 		],
 	},
-	serverExternalPackages: [ 'octokit' ]
+	serverExternalPackages: [ 'octokit', 'universal-github-app-jwt' ], 
 };
 
 export default nextConfig;
