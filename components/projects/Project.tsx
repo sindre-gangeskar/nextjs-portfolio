fdf5c241199d6a40f579b86bb7425e097ec395a5
Sindre Gangeskar <sindre.gangeskar@outlook.com>
2026-01-19 04:27:41 +0100

    Improve styling for Project component on xs breakpoint.

--- components/projects/Project.tsx
+++ components/projects/Project.tsx
@@ -1,11 +1,15 @@
 import { capitalizeString } from "@/lib/utils";
 import { GitHub, StarRounded } from "@mui/icons-material";
-import { Stack, Typography, Card, Button, Box, CardContent } from "@mui/joy";
+import { Stack, Typography, Card, Button, Box, CardContent, useTheme } from "@mui/joy";
 import { RepoType } from "@/lib/definitions";
 import { SxProps } from "@mui/material";
 import { VariantProp, ColorPaletteProp } from "@mui/joy";
 import { FaGlobe } from "react-icons/fa6";
+import { useMediaQuery } from "@mui/material";
 export default function Project({ repo, id, className, sx, variant, color }: { repo: RepoType; id?: string; className?: string; sx: SxProps; variant?: VariantProp; color?: ColorPaletteProp }) {
+	const theme = useTheme();
+	const isSmall = useMediaQuery(theme.breakpoints.down("sm"));
+	const btnSx: SxProps = { aspectRatio: { xs: 2 / 1, sm: "inherit" }, width: { xs: "60px", sm: "inherit" } };
 	return (
 		<Card id={id} className={className} variant={variant} color={color} invertedColors sx={{ ...sx }}>
 			<Typography level="title-lg">{capitalizeString(repo.name)}</Typography>
@@ -20,13 +24,17 @@ export default function Project({ repo, id, className, sx, variant, color }: { r
 						)}
 						<Box component={"span"} sx={{ display: "flex", justifyContent: "space-evenly", gap: 1, textWrap: "nowrap", width: "fit-content", alignSelf: "end" }}>
 							{repo.homepage && (
-								<Button variant="soft" color="primary" endDecorator={<FaGlobe />} component={"a"} target="_blank" href={repo.homepage}>
-									Visit Homepage
+								<Button sx={btnSx} variant="soft" color="primary" endDecorator={isSmall ? null : <FaGlobe />} component={"a"} target="_blank" href={repo.homepage}>
+									{isSmall ?
+										<FaGlobe />
+									:	"Visit Homepage"}
 								</Button>
 							)}
 							{repo.html_url && (
-								<Button variant="soft" color="neutral" endDecorator={<GitHub />} component={"a"} target="_blank" href={repo.html_url}>
-									View On Github
+								<Button sx={btnSx} variant="soft" color="neutral" endDecorator={isSmall ? null : <GitHub />} component={"a"} target="_blank" href={repo.html_url}>
+									{isSmall ?
+										<GitHub />
+									:	"View On GitHub"}
 								</Button>
 							)}
 						</Box>
