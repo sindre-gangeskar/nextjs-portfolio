1282e719bfe57511943913e6e64884530b4a007f
Sindre Gangeskar <sindre.gangeskar@outlook.com>
2026-02-16 21:34:29 +0100

    Replace nodemailer with resend

--- components/home/ContactForm.tsx
+++ components/home/ContactForm.tsx
@@ -5,14 +5,14 @@ import CheckIcon from "@mui/icons-material/Check";
 import ErrorIcon from "@mui/icons-material/Error";
 
 import ColoredTypography from "@/components/ui/ColoredTypography";
-import { EmailRounded, MailRounded, Person2Rounded, Send } from "@mui/icons-material";
+import { EmailRounded, MailRounded, Person2Rounded, Send, SubjectRounded, TopicRounded } from "@mui/icons-material";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useActionState, useEffect, useState } from "react";
 import { sendEmail } from "@/app/actions";
 export default function ContactForm() {
 	const [state, dispatch, isPending] = useActionState(sendEmail, null);
-	const [ countdown, setCountdown ] = useState(0);
+	const [countdown, setCountdown] = useState(0);
 	const theme = useTheme();
 	useGSAP(() => {
 		if (state) {
@@ -34,8 +34,8 @@ export default function ContactForm() {
 
 	return (
 		<Stack mb={10} gap={2}>
-			<Card variant="outlined" color="neutral" sx={{ mt: 5, width: "100%", mx: "auto", maxWidth: "500px", background: "none", backdropFilter: 'blur(8px)' }}>
-				<CardContent sx={{p: 2}}>
+			<Card variant="outlined" color="neutral" sx={{ mt: 5, width: "100%", mx: "auto", maxWidth: "sm", background: "none", backdropFilter: "blur(8px)" }}>
+				<CardContent sx={{ p: 2 }}>
 					<Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
 						<Typography level="h1" sx={{ alignItems: "center", alignContent: "center" }}>
 							{"Send me an "}
@@ -49,6 +49,7 @@ export default function ContactForm() {
 					<Stack id="contact-form" component={"form"} action={dispatch} gap={2} sx={{ display: "flex", flex: 1, flexDirection: "column", "input, textarea": { padding: 1.5 } }}>
 						<Input variant="soft" startDecorator={<Person2Rounded />} sx={{}} className="form-input" name="name" placeholder="Your name" required autoComplete="name"></Input>
 						<Input variant="soft" startDecorator={<EmailRounded />} sx={{}} type="email" className="form-input" name="from" placeholder="Your email address" required autoComplete="email"></Input>
+						<Input variant="soft" startDecorator={<SubjectRounded/>} sx={{}} type="text" className="form-input" name="subject" placeholder="Enter subject" required></Input>
 						<Textarea variant="soft" minRows={4} maxRows={4} sx={{ maxHeight: "96px" }} className="form-input" name="message" placeholder="Your message..." required></Textarea>
 
 						<Stack direction={"column"} id="form-input" sx={{ position: "relative", width: "fit-content", mx: "auto", alignItems: "center" }}>
--- lib/services/EmailService.ts
+++ lib/services/EmailService.ts
@@ -1,22 +1,27 @@
 import validator from 'validator';
-import nodemailer from 'nodemailer';
 import { ResponseProps } from '../definitions';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 export default class EmailService {
   static async send(formdata: FormData) {
     try {
       const name = formdata.get('name')?.toString();
       const from = formdata.get('from')?.toString();
+      const subject = formdata.get('subject')?.toString();
       const message = formdata.get('message')?.toString();
+
       if (from && !validator.isEmail(from) || !from)
         return { status: "fail", statusCode: 400, message: 'Email must be provided and follow a valid email format' } as ResponseProps;
+
+      if (!subject)
+        return { status: 'fail', statusCode: 400, message: "Subject cannot be empty" } as ResponseProps
 
       if (!name || !validator.isAlpha(name, 'en-US', { ignore: ' ' })) {
         return { status: "fail", statusCode: 400, message: 'Name must be provided and cannot contain numbers or special characters' } as ResponseProps
       }
 
-      if (process.env.NODE_ENV !== "production") await sendTestMail(name, from, message ?? "");
-      else await sendMail(name, from, message ?? "");
-
+      await sendMail(name, from, subject, message ?? "");
       return { status: "success", statusCode: 200, message: "Successfully sent email" };
     } catch (error) {
       console.error(error);
@@ -25,48 +30,23 @@ export default class EmailService {
   }
 }
 
-async function sendMail(name: string, from: string, message: string) {
-  const emailMessage = {
-    from: `${name} <${process.env.GMAIL}>`,
-    to: `Sindre Gangeskar <${process.env.EMAIL}>`,
-    subject: `Contact from portfolio`,
-    html: `
-      <p>Message: ${message}</p>
-      <p><strong>Contact info: ${from}</strong></p>
-      `,
-    text: message,
-  };
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.GMAIL,
-      pass: process.env.EMAIL_APP_PASS,
-    },
-  });
-  await transporter.sendMail(emailMessage);
-}
-async function sendTestMail(name: string, from: string, message: string) {
-  nodemailer.createTestAccount((err, account) => {
-    if (err) {
-      return console.error(err);
-    }
-    const transporter = nodemailer.createTransport({
-      host: account.smtp.host,
-      port: account.smtp.port,
-      secure: account.smtp.secure,
-      auth: { user: account.user, pass: account.pass },
-    });
-
+async function sendMail(name: string, from: string, subject: string, message: string) {
+  try {
     const emailMessage = {
-      from: `${name} <${from}>`,
-      to: `<${process.env.EMAIL}>`,
-      subject: `Contact from portfolio`,
-      text: `${message}`,
+      from: `Sindre Gangeskar Portfolio <${process.env.EMAIL!}>`,
+      to: process.env.OUTLOOK!,
+      subject: subject,
+      replyTo: `${name} <${from}>`,
+      html: `
+      <p style="font-size: 12px; color: darkorange;">${name} has tried to reach you through the contact form on your portfolio</p>
+      <hr style="border: 5px solid darkorange; border-radius: 1.25rem;"/>
+      <p style="font-size: 12px; color: #1d1d1d;">${message}</p>
+      `,
+      text: message,
     };
-
-    transporter.sendMail(emailMessage, (err, info) => {
-      if (err) { return console.error(err) }
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    });
-  });
+    await resend.emails.send(emailMessage)
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
