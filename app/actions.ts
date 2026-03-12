03fc0de01d0aa0393c2969d9617b353879c79b83
Sindre Gangeskar <sindre.gangeskar2710@outlook.com>
2026-03-12 21:29:07 +0100

    Add error handling for email server action

--- app/actions.ts
+++ app/actions.ts
@@ -8,22 +8,26 @@ import { Ratelimit } from '@upstash/ratelimit';
 const redis = new Redis({ token: process.env.UPSTASH_REDIS_REST_TOKEN, url: process.env.UPSTASH_REDIS_REST_URL })
 const rateLimiter = new Ratelimit({ redis: redis, limiter: Ratelimit.slidingWindow(3, "30 m") })
 export async function sendEmail(_: PreviousState, formdata: FormData) {
-	const headersList = await headers();
+	try {
+		const headersList = await headers();
 
-	const ip = (headersList.get('cf-connecting-ip') ?? headersList.get("x-forwarded-for") ?? '127.0.0.1').split(',')[ 0 ].trim();
-	const successKey = `contact-success:${ip}`;
-	const hasSuccessKey = await redis.get(successKey);
-	const attemptKey = `attempt:${ip}`
-	if (hasSuccessKey)
-		return { status: "fail", statusCode: 429, message: 'You have recently sent an email, please wait a while before sending another.' } as ResponseProps
+		const ip = (headersList.get('cf-connecting-ip') ?? headersList.get("x-forwarded-for") ?? '127.0.0.1').split(',')[ 0 ].trim();
+		const successKey = `contact-success:${ip}`;
+		const hasSuccessKey = await redis.get(successKey);
+		const attemptKey = `attempt:${ip}`
+		if (hasSuccessKey)
+			return { status: "fail", statusCode: 429, message: 'You have recently sent an email, please wait a while before sending another.' } as ResponseProps
 
-	const getRemainingAttempts = await rateLimiter.getRemaining(attemptKey);
-	if (getRemainingAttempts.remaining === 0) return { status: 'fail', statusCode: 429, message: 'You have made too many attempts, please try again later' } as ResponseProps
+		const getRemainingAttempts = await rateLimiter.getRemaining(attemptKey);
+		if (getRemainingAttempts.remaining === 0) return { status: 'fail', statusCode: 429, message: 'You have made too many attempts, please try again later' } as ResponseProps
 
-	const response = await EmailService.send(formdata) as ResponseProps;
-	if (response.status === "success")
-		await redis.set(successKey, "1", { ex: 600 });
-	else await rateLimiter.limit(attemptKey)
+		const response = await EmailService.send(formdata) as ResponseProps;
+		if (response.status === "success")
+			await redis.set(successKey, "1", { ex: 600 });
+		else await rateLimiter.limit(attemptKey)
 
-	return response;
+		return response;
+	} catch (error) {
+		return { status: 'fail', statusCode: 500, message: 'An unexpected error has occurred, please try again later.' } as ResponseProps
+	}
 }
