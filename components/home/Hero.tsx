e5862c7eea3fd78f25438b551a4c9003ccc28fcb
Sindre Gangeskar <sindre.gangeskar2710@outlook.com>
2026-03-31 17:19:23 +0200

    Update profile styling

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -17,12 +17,12 @@ export default function Hero() {
 		const descriptionTl = gsap.timeline();
 		const profileTl = gsap.timeline();
 
-		profileTl.set("#profile > *", { opacity: 0, x: -150 });
+		profileTl.set("#profile > *:not(#profile-description), #profile-description > *", { opacity: 0, x: -150 });
 		descriptionTl.set("#description-stack > *", { opacity: 0, filter: "blur(6px)" });
 		traitsTl.set("#traits .trait", { opacity: 0, y: -10, zIndex: -1 });
 
 		gsap.to("#profile", { opacity: 1 });
-		profileTl.to("#profile > *", { opacity: 1, x: 0, duration: 1.2, stagger: 0.5, ease: "power3.out", delay: 0.5 });
+		profileTl.to("#profile > *:not(#profile-description), #profile-description > *", { opacity: 1, x: 0, duration: 1.2, stagger: 0.5, ease: "power3.out", delay: 0.5 });
 		descriptionTl.to("#description-stack > *", {
 			delay: 0.2,
 			duration: 0.8,
@@ -52,67 +52,87 @@ export default function Hero() {
 	}, [data]);
 
 	return (
-		<Stack
-			component={"section"}
-			direction={"column"}
-			sx={{
-				position: "relative",
-				py: 2,
-			}}>
-			<Stack direction={"row"} sx={{ flexDirection: { xs: "column", md: "row" }, "& p": { lineHeight: "1.5rem", px: 2 } }}>
-				<Stack id="profile" sx={{ width: "100%", minWidth: "50%", justifyContent: "space-evenly", textAlign: { xs: "center", md: "start" }, position: "relative", opacity: 0 }}>
-					<Box id="avatar" sx={{ display: "flex", justifyContent: "center" }}>
-						{isLoading ?
-							<ProfileAvatarSkeleton />
-						:	<ProfileAvatar id="profile-image" size={325} avatar_url={data?.avatar_url} sx={{ display: { xs: "none", md: "block" }, opacity: 0 }} />}
-					</Box>
-					<Typography level="h1" textAlign={"center"} sx={{ opacity: 0 }}>
-						I am <ColoredTypography level="h1">Sindre Gangeskar</ColoredTypography>
-					</Typography>
-					<Typography textAlign={"center"} sx={{ opacity: 0 }}>
-						A passionate back-end and full-stack developer from Norway
-					</Typography>
-				</Stack>
-				<Stack sx={{ justifyContent: "space-between" }}>
-					<Box
-						id="traits"
-						sx={{ background: "transparent", borderRadius: 0, display: "flex", flexWrap: "wrap", flexDirection: "row", width: "100%", flexShrink: 1, p: 3, gap: 1, justifyContent: "center", opacity: 0 }}>
-						<Chip slotProps={{ label: { id: "chip-team-player" } }} variant="soft" className="trait" color="primary" component={"span"} sx={{ height: "fit-content" }}>
-							Team Player
-						</Chip>
-						<Chip slotProps={{ label: { id: "chip-adaptable" } }} variant="soft" className="trait" color="neutral" component={"span"} sx={{ height: "fit-content" }}>
-							Adaptable
-						</Chip>
-						<Chip slotProps={{ label: { id: "chip-critical-thinker" } }} variant="soft" className="trait" color="danger" component={"span"} sx={{ height: "fit-content" }}>
-							Critical Thinker
-						</Chip>
-						<Chip slotProps={{ label: { id: "chip-proactive" } }} variant="soft" className="trait" color="warning" component={"span"} sx={{ height: "fit-content" }}>
-							Proactive
-						</Chip>
-						<Chip slotProps={{ label: { id: "chip-detail-oriented" } }} variant="soft" className="trait" color="primary" component={"span"} sx={{ height: "fit-content" }}>
-							Detail-Oriented
-						</Chip>
-						<Chip slotProps={{ label: { id: "chip-enthusiastic" } }} variant="soft" className="trait" color="success" component={"span"} sx={{ height: "fit-content" }}>
-							Enthusiastic
-						</Chip>
-						<Chip slotProps={{ label: { id: "chip-passionate" } }} variant="soft" className="trait" color="danger" component={"span"} sx={{ height: "fit-content" }}>
-							Passionate
-						</Chip>
-					</Box>
-					<Box id="description" component={"div"} sx={{ overflow: "hidden" }}>
-						<Stack id="description-stack" gap={2}>
-							<Typography level="title-sm">
-								I am a back-end developer with full-stack capabilities skilled in building robust and scalable RESTful applications with modern front-end frameworks. <br />
-							</Typography>
-							<Typography level="title-sm">
-								I specialize in developing efficient APIs, managing databases and implementing robust authentication methods. I prioritize clean and maintainable code and thorough API documentation.
-							</Typography>
-							<Typography level="title-sm">I am curious and eager to learn technologies for both front-end and back-end to apply in a professional setting.</Typography>
-						</Stack>
-					</Box>
-					<Links />
-				</Stack>
+	<Stack
+		component={"section"}
+		direction={"row"}
+		sx={theme => ({
+			position: "relative",
+			zIndex: 0,
+			p: 3,
+			mt: 3,
+			mx: 0,
+			overflow: "hidden",
+			flexDirection: { xs: "column", md: "row" },
+			"& p": { lineHeight: "1.5rem" },
+			"&::before": {
+				content: '""',
+				display: "block",
+				width: "100%",
+				position: "absolute",
+				inset: 0,
+				zIndex: -1,
+				borderRadius: "0.75rem",
+				backgroundColor: `color-mix(${theme.vars.palette.background.surface} 40%, transparent)`,
+
+				outline: `1px solid ${theme.vars.palette.primary.outlinedBorder}`,
+				outlineOffset: "-1px",
+				backdropFilter: "blur(2px)",
+			},
+		})}>
+		<Stack id="profile" sx={{ width: "100%", minWidth: "50%", justifyContent: "space-evenly", textAlign: { xs: "center", md: "start" }, position: "relative", opacity: 0 }}>
+			<Box id="avatar" sx={{ display: "flex", justifyContent: "center" }}>
+				{isLoading ?
+					<ProfileAvatarSkeleton />
+				:	<ProfileAvatar id="profile-image" size={325} avatar_url={data?.avatar_url} sx={{ display: { xs: "none", md: "block" }, opacity: 0 }} />}
+			</Box>
+			<Stack id="profile-description" gap={0} p={0}>
+				<Typography level="h1" textAlign={"center"} sx={{ opacity: 0 }}>
+					I am <ColoredTypography level="h1">Sindre Gangeskar</ColoredTypography>
+				</Typography>
+				<Typography textAlign={"center"} sx={{ opacity: 0 }}>
+					A passionate back-end and full-stack developer from Norway
+				</Typography>
 			</Stack>
 		</Stack>
-	);
+		<Stack sx={{ justifyContent: "space-between" }}>
+			<Box
+				id="traits"
+				sx={{ background: "transparent", borderRadius: 0, display: "flex", flexWrap: "wrap", flexDirection: "row", width: "100%", flexShrink: 1, p: 3, gap: 1, justifyContent: "center", opacity: 0 }}>
+				<Chip slotProps={{ label: { id: "chip-team-player" } }} variant="soft" className="trait" color="primary" component={"span"} sx={{ height: "fit-content" }}>
+					Team Player
+				</Chip>
+				<Chip slotProps={{ label: { id: "chip-adaptable" } }} variant="soft" className="trait" color="neutral" component={"span"} sx={{ height: "fit-content" }}>
+					Adaptable
+				</Chip>
+				<Chip slotProps={{ label: { id: "chip-critical-thinker" } }} variant="soft" className="trait" color="danger" component={"span"} sx={{ height: "fit-content" }}>
+					Critical Thinker
+				</Chip>
+				<Chip slotProps={{ label: { id: "chip-proactive" } }} variant="soft" className="trait" color="warning" component={"span"} sx={{ height: "fit-content" }}>
+					Proactive
+				</Chip>
+				<Chip slotProps={{ label: { id: "chip-detail-oriented" } }} variant="soft" className="trait" color="primary" component={"span"} sx={{ height: "fit-content" }}>
+					Detail-Oriented
+				</Chip>
+				<Chip slotProps={{ label: { id: "chip-enthusiastic" } }} variant="soft" className="trait" color="success" component={"span"} sx={{ height: "fit-content" }}>
+					Enthusiastic
+				</Chip>
+				<Chip slotProps={{ label: { id: "chip-passionate" } }} variant="soft" className="trait" color="danger" component={"span"} sx={{ height: "fit-content" }}>
+					Passionate
+				</Chip>
+			</Box>
+			<Box id="description" component={"div"} sx={{ overflow: "hidden" }}>
+				<Stack id="description-stack" gap={3}>
+					<Typography level="title-sm">
+						I am a back-end developer with full-stack capabilities skilled in building robust and scalable RESTful applications with modern front-end frameworks. <br />
+					</Typography>
+					<Typography level="title-sm">
+						I specialize in developing efficient APIs, managing databases and implementing robust authentication methods. I prioritize clean and maintainable code and thorough API documentation.
+					</Typography>
+					<Typography level="title-sm">I am curious and eager to learn technologies for both front-end and back-end to apply in a professional setting.</Typography>
+				</Stack>
+			</Box>
+			<Links />
+		</Stack>
+	</Stack>
+);
 }
